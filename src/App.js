a5583dbdf4db86559a629434cbdf962de5b1b3cb
Francisco Ricardo <ricardo.souza1910@gmail.com>
2021-01-29 21:10:16 -0400

    Projeto React
    Copiado todo projeto react para o novo repositorio

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import Comentario from './componentes/Comentario'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Meu Projeto</h1>
+     <Comentario/>
+     <Comentario/>
+     <Comentario/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Comentario.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Comentario.css';
+
+//JSX
+const Comentario = () => (
+    <div className="Comentario"> 
+        <div>Ricardo:</div>
+        <div>Olá! Tudo bem</div>
+    </div>
+);
+
+export default Comentario;

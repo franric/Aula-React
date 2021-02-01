32f2b9bba4cdb15b4f6f97da484c0a93da997496
Francisco Ricardo <ricardo.souza1910@gmail.com>
2021-02-01 09:51:53 -0400

    Error aula react
    Ate aqui deu um error desconhecido

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
+import React, { Component } from 'react';
 import './App.css';
 
 import Comentario from './componentes/Comentario'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Meu Projeto</h1>
-      <Comentario nome="Ricardo" email="ricardo@email.com" data={new Date(2020, 3, 19)}>
-        Ola Tudo bem?
-      </Comentario>
-      <Comentario nome="Maria" email="maria@email.com" data={new Date(2020, 3, 21)} >
-        Ola, tudo bem sim.
-      </Comentario>
-      <Comentario nome="Ricardo" email="ricardo@email.com" data={new Date(2020, 3, 21)} >>
-        Hoje vamos desenvolver qual projeto ?
-      </Comentario>
-      <Comentario nome="Maria" email="maria@email.com" data={new Date(2020, 3, 21)} >
-        Vamos Conversar com a equipe toda para poder definimos. 
-      </Comentario>
-        
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    comentarios: [
+      {
+        nome: "Ricardo",
+        email: "ricardo@email.com",
+        data: new Date(2020, 3, 19),
+        mensagem: 'Olá, tudo Bem?'
+      },
+      {
+        nome: "Maria",
+        email: "maria@email.com",
+        data: new Date(2020, 3, 22),
+        mensagem: 'Olá, tudo bem sim'
+      }
+    ]
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Meu Projeto</h1>
+
+        {this.state.comentarios.map(comentario => (
+            console.log(comentario)
+          ))}
+
+        <Comentario nome="Ricardo" email="ricardo@email.com" data={new Date(2020, 3, 19)} >
+          Ola Tudo bem?
+        </Comentario>
+        <Comentario nome="Maria" email="maria@email.com" data={new Date(2020, 3, 21)} >
+          Ola, tudo bem sim.
+        </Comentario>        
+          
+      </div>
+    );
+  }  
 }
 
 export default App;
--- src/componentes/Comentario.js
+++ src/componentes/Comentario.js
@@ -3,13 +3,14 @@ import React from 'react';
 import './Comentario.css';
 
 //JSX
-const Comentario = props =>  (
+const Comentario = props =>  {
+    
     <div className="Comentario"> 
         <h2>{props.nome}:</h2>
         <p>{props.email}</p>
         <p>{props.children}</p>
         <p>{props.data.toString()}</p>
     </div>
-);
+}
 
 export default Comentario;
